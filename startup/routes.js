548281b1de7abeb610607e649877fabf127f028f
Brett Knadle <brett182x@gmail.com>
2021-02-21 17:08:58 -0800

    adding cors

--- startup/routes.js
+++ startup/routes.js
@@ -9,12 +9,21 @@ const projects = require("../routes/projects");
 
 module.exports = function (app) {
   if (app.get("env") === "development") {
+    app.use(cors());
     app.use(morgan("tiny"));
     winston.info("Morgan enabled...");
   }
 
+  if (app.get("env") === "production") {
+    var corsOptions = {
+      origin: "https://projectiviti.com",
+      optionsSuccessStatus: 200,
+    };
+    app.use(cors(corsOptions));
+  }
+
   app.use(express.json());
-  app.use(cors());
+  app.use(cors(corsOptions));
   app.use(helmet());
   app.use("/api/tasks", tasks);
   app.use("/api/projects", projects);
