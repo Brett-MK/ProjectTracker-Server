a2cab3c8d5d632e05896dd666aa5a4c3c7e68da9
Brett Knadle <brett182x@gmail.com>
2021-02-21 17:20:53 -0800

    trying to fix cors in production

--- startup/routes.js
+++ startup/routes.js
@@ -9,21 +9,12 @@ const projects = require("../routes/projects");
 
 module.exports = function (app) {
   if (app.get("env") === "development") {
-    app.use(cors());
     app.use(morgan("tiny"));
     winston.info("Morgan enabled...");
   }
 
-  if (app.get("env") === "production") {
-    var corsOptions = {
-      origin: "https://projectiviti.com",
-      optionsSuccessStatus: 200,
-    };
-    app.use(cors(corsOptions));
-  }
-
   app.use(express.json());
-  app.use(cors(corsOptions));
+  app.use(cors());
   app.use(helmet());
   app.use("/api/tasks", tasks);
   app.use("/api/projects", projects);
